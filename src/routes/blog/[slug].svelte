c9675e652b44a65836354c52152dcd496e5df763
Diego <diegoartiles13@gmail.com>
2020-09-19 16:28:51 -0300

    add slug meta info

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -73,19 +73,19 @@
 <svelte:head>
 	<title>{post.title}</title>
 
-	<meta name="description" content="Blog de Diego Artiles">
+	<meta name="description" content={post.title}>
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@dartilesm" />
 	<meta name="twitter:creator" content="@dartilesm" />
-	<meta name="twitter:title" content="Diego Artiles" />
-	<meta name="twitter:description" content="Blog de Diego Artiles" />
-	<meta name="twitter:image" content="https://www.filepicker.io/api/file/vPMx0ySXm2L1l53rR77Q" />
+	<meta name="twitter:title" content="Dartiles.dev" />
+	<meta name="twitter:description" content={post.title} />
+	<meta name="twitter:image" content={post.feature_image} />
 	
-	<meta name="og:title" content="Diego Artiles" />
-	<meta name="og:site_name" content="Diego Artiles" />
-	<meta name="og:description" content="Blog de Diego Artiles" />
-	<meta name="og:image" content="https://www.filepicker.io/api/file/vPMx0ySXm2L1l53rR77Q" />
+	<meta name="og:title" content="Dartiles.dev" />
+	<meta name="og:site_name" content="Dartiles.dev" />
+	<meta name="og:description" content={post.title} />
+	<meta name="og:image" content={post.feature_image} />
 	<meta name="og:url" content="https://blog.dartiles.live" />
 	<meta name="og:locale" content="es_ES">
 	<meta name="og:type" content="article">
