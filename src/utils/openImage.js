2d370dce4ee5cb6bf3acefc404cfdd5a856659bf
Diego Artiles <diegoartiles13@gmail.com>
2020-11-05 22:00:43 -0300

    fix ga on open image

--- src/utils/openImage.js
+++ src/utils/openImage.js
@@ -1,8 +1,8 @@
 import { sendEventGA } from "./analytics"
 
 const toggleImage = element => {
-    sendEventGA('image', 'open-image', 'post-image')
     if (!element.className.includes('opened')) {
+        sendEventGA('image', 'open-image', 'post-image')
         element.closest('figure').classList.add('opened')
         element.classList.add('opened')
     } else {
