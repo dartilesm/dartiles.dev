a0b6915a370fe63bbb57b9fef8469dcb924be076
Diego Artiles <diego45882@hotmail.com>
2022-03-04 18:16:23 -0300

    fix (rss): fix rss file

--- src/bin/seo.js
+++ src/bin/seo.js
@@ -15,58 +15,55 @@ const createRss = (data) => {
     .map((item) => {
       const pubDate = getDate(item.createdAt);
       return `
-        <item>
-            <title>
-                <![CDATA[${item.title}]]>
-            </title>
-            <link>
-                ${blog.url}/blog/${item.slug}
-            </link>
-            <guid>
-                ${blog.url}/blog/${item.slug}
-            </guid>
-            <description>
-                <![CDATA[ ${item.desc} ]]>
-            </description>
-            <pubDate>
-                ${pubDate}
-            </pubDate>
-            <media:content url="${blog.url}/${item.image}" medium="image" />
-        </item>
-        `;
+                <item>
+                    <title>
+                        <![CDATA[${item.title}]]>
+                    </title>
+                    <link>
+                        ${blog.url}/blog/${item.slug}
+                    </link>
+                    <guid>
+                        ${blog.url}/blog/${item.slug}
+                    </guid>
+                    <description>
+                        <![CDATA[ ${item.desc} ]]>
+                    </description>
+                    <pubDate>
+                        ${pubDate}
+                    </pubDate>
+                    <media:content url="${blog.url}/${item.image}" medium="image" />
+                </item>`;
     })
     .join("");
 
   const template = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">
-
-    <channel>
-    <title>
-        <![CDATA[${blog.title}]]>
-    </title>
-    <description>
-        <![CDATA[${blog.description}]]>
-    </description>
-    <image>
-        <url>${blog.favicon}</url>
-        <title>
-            <![CDATA[${blog.title}]]>
-        </title>
-        <title>
-            ${blog.url}
-        </title>
-    </image>
-    <generator>
-        Svelte
-    </generator>
-    <lastBuildDate>
-        ${getDate()}
-    </lastBuildDate>
-    <atom:link href="${blog.url}/rss.xml" rel="self" type="application/rss+xml" />
-    <ttl>68</ttl>
-    ${parseItems}
-    </channel>
-
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
+        <channel>
+            <title>
+                <![CDATA[${blog.title}]]>
+            </title>
+            <description>
+                <![CDATA[${blog.description}]]>
+            </description>
+            <image>
+                <url>${blog.favicon}</url>
+                <title>
+                    <![CDATA[${blog.title}]]>
+                </title>
+                <title>
+                    ${blog.url}
+                </title>
+            </image>
+            <generator>
+                Svelte
+            </generator>
+            <lastBuildDate>
+                ${getDate()}
+            </lastBuildDate>
+            <atom:link href="${blog.url}/rss.xml" rel="self" type="application/rss+xml" />
+            <ttl>68</ttl>
+                ${parseItems}
+        </channel>
     </rss>`;
 
     fs.writeFileSync("./static/rss.xml", template);
